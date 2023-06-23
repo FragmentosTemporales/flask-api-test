bb807ab63ba7096815bc719a9e0c3a744536a7c9
Cristian Rivera <cristian.rivera3284@gmail.com>
2023-06-23 01:59:07 -0400

    Actualización method=DELETE

--- app.py
+++ app.py
@@ -20,9 +20,5 @@ app.register_blueprint(users)
 with app.app_context():
     db.create_all()
 
-@app.route("/")
-def home():
-    return render_template('index.html')
-
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port="8080")
--- routes/products.py
+++ routes/products.py
@@ -1,13 +1,14 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.product import Product
 from utils.db import db
 
 products = Blueprint("products", __name__)
 
 
-@products.route("/products")
+@products.route("/")
 def home():
-    return render_template("products.html")
+    products = Product.query.all()
+    return render_template("products.html", products=products)
 
 
 @products.route("/products", methods=["POST"])
@@ -16,11 +17,25 @@ def add_product():
     product.name = request.form["name"]
     product.brand = request.form["brand"]
     product.avatar = request.form["avatar"]
-    print(product)
     db.session.add(product)
     db.session.commit()
 
-    return redirect('/')
+    return redirect(url_for('products.home'))
+
+
+@products.route("/delete/<id>")
+def delete(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    
+    return redirect(url_for('products.home'))
+
+
+@products.route("/update/<id>")
+def update(id):
+    print(id)
+    return render_template("update.html")
 
 
 if __name__ == "__main__":
