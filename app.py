48670c3c24e4c6ce6d5f767e9e01fb07ec8f86d3
Cristian Rivera <cristian.rivera3284@gmail.com>
2023-06-22 21:15:15 -0400

    Creación de Templates, Routes para products y users. db se crea en utils/db.py y se importa en app.py y models/models.py.

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.products import products
-from models.models import db
+from routes.users import users
+from flask_migrate import Migrate
+from utils.db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db.init_app(app)
+
+migrate = Migrate(app, db)
 
 app.register_blueprint(products)
+app.register_blueprint(users)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="localhost", port="8080")
--- routes/products.py
+++ routes/products.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 products = Blueprint('products', __name__)
 
-@products.route("/")
+@products.route("/products")
 def home():
-    return render_template('index.html')
+    return render_template('products.html')
